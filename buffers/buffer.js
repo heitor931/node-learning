599f3877af3e36f63087599e1bc5aea709f910be
Heitor Paulo <heitorino1@gmail.com>
2025-07-23 17:11:16 +0100

    Added more stuff

--- buffers/buffer.js
+++ buffers/buffer.js
@@ -1,8 +1,18 @@
-const {Buffer} = require("buffer")
+import { pipeline } from "stream"
+import fs from "fs"
 
-const myMemory = Buffer.alloc(5000000000)
-console.log(myMemory)
+pipeline(
+  fs.createReadStream("MongoDB.mp4"),
+  fs.createWriteStream("/engineering/MongoDB.mp4"),
+  (err) => {
+    if (err) {
+      console.log(err);
+  
+    } else {
+      console.log("Pipeline succeded");
+  
+    }
+  }
+)
 
-for (let i = 0; i < myMemory.length; i++) {
-  myMemory[i] = Math.floor(Math.random() * 256)
-}
+
